4350fa55bc21160044f001d8b139e3d09ab70e67
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-01-08 22:32:25 +0100

    Implement creating moving and rotating blocks

---
+++ src/main/acme/frontend/tetris/block.cljs
@@ -0,0 +1,40 @@
+(ns acme.frontend.tetris.block
+  (:require [acme.frontend.tetris.point :as point]))
+
+(def shapes [:i :t :o :l :j :z :s])
+
+(defn create [{:keys [rotation location]
+               :or {rotation 0
+                    location {:x 5 :y 1}}}]
+  {:shape (rand-nth shapes)
+   :rotation rotation
+   :location location})
+
+(defn move-right [tetro]
+  (update tetro :location point/right))
+
+(defn move-left [tetro]
+  (update tetro :location point/left))
+
+(defn move-down [tetro]
+  (update tetro :location point/down))
+
+(defn rotate [tetro]
+  (update tetro :rotation #(mod (+ % 90) 360)))
+
+(comment
+  (def x (create {:location {:x 1 :y 1}}))
+  (move-right x)
+  (move-down x)
+  (move-left x)
+  (defn- rotate-degrees
+    [degrees]
+    (if (= degrees 270) 0 (+ degrees 90)))
+
+  (rotate-degrees 180)
+  (rotate-degrees 270)
+  (rotate x)
+  (defn- rotate-old [tetro]
+    (update tetro :rotation rotate-degrees))
+  (rotate-old x)
+  (repeatedly 10 #(rand-nth shapes)))
